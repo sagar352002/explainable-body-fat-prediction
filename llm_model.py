15b72abf54a77cd479287bfab3406ffbbe1d30f8
Sagar Kumar <sagarkumarsoh@gmail.com>
2026-05-02 13:25:52 +0530

    Update llm_model.py

--- llm_model.py
+++ llm_model.py
@@ -21,7 +21,7 @@ def generate_fitness_summary(llm_input: dict) -> list:
     prompt = body_fitness_prompt_template(llm_input)
 
     llm = ChatOpenAI(
-        model="arcee-ai/trinity-large-preview:free",
+        model="arcee-ai/trinity-mini",
         api_key=os.getenv("OPENROUTER_API_KEY"),
         base_url="https://openrouter.ai/api/v1",
         temperature=0.6
